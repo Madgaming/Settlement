8e4b50742a90766cbc7b47ba5851d52ec649967e
Daniel <ZetaetaDaniel@gmail.com>
2012-05-07 15:54:24 +0100

    Bypass-settees are notified

--- src/main/java/net/zetaeta/settlement/commands/settlement/Bypass.java
+++ src/main/java/net/zetaeta/settlement/commands/settlement/Bypass.java
@@ -45,15 +45,18 @@ public class Bypass extends SettlementCommand {
             if (cArgs.hasBooleanFlag("on") || (args.length > 1 && args[1].equalsIgnoreCase("on"))) {
                 target.setBypass(true);
                 SettlementMessenger.sendSettlementMessage(sender, "§a  Settlement protection bypass enabled for §b" + target.getName());
+                SettlementMessenger.sendSettlementMessage(target.getPlayer(), "§a  Settlement protection bypass enabled!");
                 return true;
             }
             if (cArgs.hasBooleanFlag("off") || (args.length > 1 && args[1].equalsIgnoreCase("off"))) {
                 target.setBypass(false);
                 SettlementMessenger.sendSettlementMessage(sender, "§a  Settlement protection bypass disabled for §b" + target.getName());
+                SettlementMessenger.sendSettlementMessage(target.getPlayer(), "§a  Settlement protection bypass disabled!");
                 return true;
             }
             target.setBypass(!target.hasBypass());
             SettlementMessenger.sendSettlementMessage(sender, SettlementUtil.concatString(80, "§a  Settlement protection bypass for §b", target.getName(), " §achanged to ", String.valueOf(target.hasBypass()), "!"));
+            SettlementMessenger.sendSettlementMessage(target.getPlayer(), "§a  Settlement protection bypass changed to " + String.valueOf(target.hasBypass()) + "!");
             return true;
         }
         if (cArgs.hasBooleanFlag("on") || (args.length > 0 && args[0].equalsIgnoreCase("on"))) {
